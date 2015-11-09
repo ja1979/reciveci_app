3955ff9370ec241dff7b9dd8d974b099357521be
Jorge Aguilar <jorge.aguilar.ec@gmail.com>
2015-11-08 20:29:38 -0500

    Improved news and map

--- www/scripts/map-controller.js
+++ www/scripts/map-controller.js
@@ -1,11 +1,20 @@
 
-myApp.controller('MapController', function() {
+myApp.controller('MapController', function($scope) {
 
 
     //alert('MapController');
     //console.log('MapController');
+
+
+    $scope.openProfile = function() {
+      console.log('openProfile');
+      $scope.mapNavigator.pushPage('profile.html');
+    };
+
+
+
             
-    var map = L.map('map').setView([-0.181451, -78.480030], 16);
+    var map = L.map('map').setView([-0.18080, -78.48003], 16);
     
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
       attribution: 'Map data &copy; <a href="http://openo streetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -21,34 +30,40 @@ myApp.controller('MapController', function() {
     }
 
 
+
+
     function onEachFeature(feature, layer) {
-        // does this feature have a property named popupContent?
-        if (feature.properties && feature.properties.schedule) {
-            //console.log("Setting popup...");
-            layer.bindPopup(feature.properties.popup);
-        }
+      if (feature.properties && feature.properties.schedule) {
+
+        var container = $('<div />');
+
+        // Delegate all event handling for the container itself and its contents to the container
+        container.on('click', '.profileLink', function() {
+          $scope.openProfile();
+        });
+
+        // Insert whatever you want into the container, using whichever approach you prefer
+        container.html("<a class='profileLink' href='#'><div class='centered'><img class='wastepickerpic' src='" + feature.properties["waste_picker-image_url"] + "'/></div>" +
+                    "<div class='centered'>" +
+                    feature.properties["waste_picker-name"] + "</div></a><br>");
+        container.append(feature.properties["schedule"]);
+
+        // Insert the container into the popup
+        layer.bindPopup(container[0]);
+
+      }
     }
 
 
     var routes;
 
-    $.getJSON("http://api-reciveci.rhcloud.com/map/routes.json", function(data) {
+    //$.getJSON("http://api-reciveci.rhcloud.com/map/routes.json", function(data) {      
+    $.getJSON("http://192.168.10.175:5000/map/routes.json", function(data) {
       routes = data ;
       //console.log(routes);
       L.geoJson(routes, {style: style, onEachFeature: onEachFeature}).addTo(map);
     });
 
-
-    var popup = L.popup();
-    
-    function onMapClick(e)  {
-        popup
-            .setLatLng(e.latlng)
-            .setContent(e.latlng.lng + ", " + e.latlng.lat)
-            .openOn(map);
-    }
-
-    map.on('click', onMapClick);
   
 });
     
--- www/scripts/news-controller.js
+++ www/scripts/news-controller.js
@@ -1,23 +1,19 @@
 myApp.controller('NewsController', function($scope, $news) {
 
-    //console.log('NewsController');
-    
     
     $scope.news = [];
-    //$scope.selectedItem = {title: "Prueba"};
     
     $news.last(function(news) {
         $scope.news = news;
     });
 
 
-    $scope.showDetail = function(index){ 
-        console.log(index);
-        var selectedItem = $scope.news[index];
-        console.log(selectedItem.title);
+    $scope.showDetail = function(newObj){ 
+        //console.log(newObj.title);
         //console.log($scope.news[index]);
         //$scope.selectedItem = selectedItem;
-        $scope.newsNavigator.pushPage('new.html', selectedItem);
+        $scope.newsNavigator.pushPage('new.html');
+        $scope.currentNew = newObj;
     }
 
   
