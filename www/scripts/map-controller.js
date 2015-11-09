a3e192b0e83761fccd0bdf6fd16759bbb4b32d37
Jorge Aguilar <jorge.aguilar.ec@gmail.com>
2015-11-08 22:38:31 -0500

    GUI improvements

--- www/scripts/map-controller.js
+++ www/scripts/map-controller.js
@@ -13,9 +13,8 @@ myApp.controller('MapController', function($scope) {
 
 
 
-            
     var map = L.map('map').setView([-0.18080, -78.48003], 16);
-    
+        
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
       attribution: 'Map data &copy; <a href="http://openo streetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18,
@@ -57,8 +56,8 @@ myApp.controller('MapController', function($scope) {
 
     var routes;
 
-    //$.getJSON("http://api-reciveci.rhcloud.com/map/routes.json", function(data) {      
-    $.getJSON("http://192.168.10.175:5000/map/routes.json", function(data) {
+    $.getJSON("http://api-reciveci.rhcloud.com/map/routes.json", function(data) {      
+    //$.getJSON("http://192.168.10.175:5000/map/routes.json", function(data) {
       routes = data ;
       //console.log(routes);
       L.geoJson(routes, {style: style, onEachFeature: onEachFeature}).addTo(map);
