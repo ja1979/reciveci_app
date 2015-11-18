35ddd41232ee75b94cc07ef7748b07d025f87419
Jorge Aguilar <jorge.aguilar.ec@gmail.com>
2015-11-18 09:10:45 -0500

    Background images

--- www/scripts/news-controller.js
+++ www/scripts/news-controller.js
@@ -2,18 +2,67 @@ myApp.controller('NewsController', function($scope, $news, $timeout) {
 
     
     $scope.news = [];
+    $scope.success = true;
+
+    var initialLoad = function() {
+
+        
+        $timeout(function(){
+            $scope.success = true;
+            modal.show();
+            $news.last(
+                function(news) {
+                    $scope.news = news;
+                    modal.hide();
+                },
+                function(error) {
+                    console.log(error);
+                    $scope.success = false;
+                    modal.hide();
+                    //alert('Error: ');
+                    /*
+                    ons.notification.alert({
+                        title: 'Error',
+                        message: 'Lo sentimos, no se pudo completar la consulta  :('
+                    });
+                    */
+                }
+            );
+
+
+        },100);
+
+    }
+
+    initialLoad();
+
     
-    $timeout(function(){
-        modal.show();
-        $news.last(function(news) {
-            $scope.news = news;
-            modal.hide();
-        });
+    $scope.load = function($done) {
+        $scope.success = true;
+        $news.last(
+            function(news) {
+                $scope.news = news;
+                $done();
+            },
+            function(error) {
+                $scope.success = false;
+                $done();
+                /*
+                ons.notification.alert({
+                    title: 'Error',
+                    message: 'Lo sentimos, no se pudo completar la consulta  :('
+                });
+                */
 
-    },100);
+            }
+        );
+     
+    };
 
 
 
+
+  
 
 
     $scope.showDetail = function(newObj){ 
--- www/scripts/news-service.js
+++ www/scripts/news-service.js
@@ -1,9 +1,10 @@
+/*
 myApp.factory('$news', function($http) {
   //var API_ROOT = 'http://api-reciveci.rhcloud.com/';
   //var API_ROOT = 'http://192.168.10.104:5000/';
   //var API_ROOT = 'http://192.168.43.240:5000/';
   //var API_ROOT = 'http://localhost:5000/';
-  var API_ROOT = 'http://192.168.43.240:5000/';
+  var API_ROOT = 'http://192.168.0.107:5000/';
 
   return {
     last: function(callback){
@@ -11,3 +12,19 @@ myApp.factory('$news', function($http) {
     }
   };
 });
+*/
+
+myApp.factory('$news', [ '$resource', function($resource) {
+  //var API_ROOT = 'http://api-reciveci.rhcloud.com/';
+  //var API_ROOT = 'http://192.168.10.104:5000/';
+  //var API_ROOT = 'http://192.168.43.240:5000/';
+  //var API_ROOT = 'http://localhost:5000/';
+  var API_ROOT = 'http://192.168.0.107:5000/';  
+  return $resource(API_ROOT + "articles_last.json", {}, {
+      last : {
+          method : "GET",
+          isArray : true,
+          timeout : 10000
+      }
+    });
+} ])
