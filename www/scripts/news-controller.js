05c5728c6f280d9844b1f83225cecf9593d656e2
Jorge Aguilar <jorge.aguilar.ec@gmail.com>
2016-02-28 19:28:09 -0500

    News section. Only one call to services.

--- www/scripts/news-controller.js
+++ www/scripts/news-controller.js
@@ -4,89 +4,50 @@ myApp.controller('NewsController', function($scope, $news, $timeout) {
     $scope.news = [];
     $scope.success = true;
 
-    var initialLoad = function() {
 
+    var loadData = function($done) {
+      $scope.success = true;
+      modal.show();
+      $news.last(
+          function(news) {
+              // Store in local store
+              var newsData = JSON.stringify(news);
+              localStorage.setItem('newsData', newsData);
 
-        $timeout(function(){
-            $scope.success = true;
-            modal.show();
-            $news.last(
-                function(news) {
+              $scope.news = news;
+              modal.hide();
+              if ($done)
+                $done();
+          },
+          function(error) {
+              console.log(error);
 
-                    var newsData = JSON.stringify(news);
-                    console.log(news);
+              // Retrieve data from cache
+              var news = JSON.parse(localStorage.getItem('newsData'));
 
-                    localStorage.setItem('newsData', newsData);
-
-                    console.log(localStorage.length);
-                    // console.log(localStorage["newsData"]);
-
-
-                    var foo = JSON.parse(localStorage.getItem('newsData'));
-
-                    console.log(foo);
-
-                    $scope.news = foo;
-                    // $scope.news = news;
-                    modal.hide();
-                },
-                function(error) {
-                    console.log(error);
-
-                    // console.log("Looking for data in cache");
-                    var news = JSON.parse(localStorage.getItem('newsData'));
-
-                    if (news != null) {
-                      $scope.news = news;
-                    } else {
-                      $scope.success = false;
-                    }
-
-                    modal.hide();
-                    //alert('Error: ');
-                    /*
-                    ons.notification.alert({
-                        title: 'Error',
-                        message: 'Lo sentimos, no se pudo completar la consulta  :('
-                    });
-                    */
-                }
-            );
-
-
-        },100);
-
-    }
-
-    initialLoad();
-
-
-    $scope.load = function($done) {
-        $scope.success = true;
-        $news.last(
-            function(news) {
+              if (news != null) {
                 $scope.news = news;
-                $done();
-            },
-            function(error) {
+              } else {
                 $scope.success = false;
+              }
+              modal.hide();
+              if ($done)
                 $done();
-                /*
-                ons.notification.alert({
-                    title: 'Error',
-                    message: 'Lo sentimos, no se pudo completar la consulta  :('
-                });
-                */
-
-            }
-        );
-
+          }
+      );
     };
 
 
+  // Initial load
+  $timeout(function() {
+      loadData();
+    },100);
 
 
 
+    $scope.loadDataPull = function($done) {
+        loadData($done);
+    };
 
 
     $scope.showDetail = function(newObj){
