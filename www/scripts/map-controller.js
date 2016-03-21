72d53005ed162207951b75377dbbdfd7e30f359a
Jorge Aguilar <jorge.aguilar.ec@gmail.com>
2016-03-20 21:04:42 -0500

    Issues 45,46,48,50. Version 0.2.1

--- www/scripts/map-controller.js
+++ www/scripts/map-controller.js
@@ -1,175 +1,158 @@
 
 myApp.controller('MapController', function($scope, $timeout) {
 
-
-  // $scope.pilotNotification = function(event, material) {
-  //     // console.log("In pilotNotification");
-  //     console.log(event);
-  //     // if (event.enterPage.name == null) {
-  //       ons.notification.alert({
-  //         message: 'Durante el piloto de ReciVeci, solamente estará disponible en el sector mostrado en el mapa. Pronto estaremos cubriendo más sectores y ciudades.',
-  //         modifier: material ? 'material' : undefined
-  //       });
-  //     // }
-  //   }
+  var map;
+  var geojsonLayer;
+  var popup;
 
 
-
-    //alert('MapController');
-    // console.log('MapController');
-
-
-    $scope.openProfile = function(feature) {
-      // console.log(feature.properties["waste_picker-name"]);
-      $scope.mapNavigator.pushPage('profile.html');
-      $scope.currentWastePicker = feature.properties;
-    };
-
-    var map;
-    var geojsonLayer;
-    var popup;
-
-    showHideLayers = function(cb) {
-      // console.log(geojsonLayer);
-      if (cb.checked) {
-        geojsonLayer.addTo(map);
-        // var counter = 0;
-        // geojsonLayer.eachLayer(function(feature) {
-        //   feature.openPopup();
-        // });
-
-        var mapAlertExecuted = localStorage.getItem('mapAlertExecuted');
-        // console.log("tourExecuted: " + tourExecuted);
-        if (mapAlertExecuted == null) {
-          localStorage.setItem('mapAlertExecuted', 'Y');
-          popup = L.popup()
-                      .setLatLng([-0.18292634404976632, -78.47974061965942])
-                      .setContent("Haz un tab sobre cada línea para</br>conocer a <strong>l@s reciclador@s<strong>")
-                      .openOn(map);
-        }
+  $scope.openProfile = function(feature) {
+    $scope.mapNavigator.pushPage('profile.html');
+    $scope.currentWastePicker = feature.properties;
+  };
 
 
-
-      }
-      else {
-        map.closePopup(popup);
-        map.removeLayer(geojsonLayer);
-      }
+  showHideLayers = function(cb) {
+    // console.log(geojsonLayer);
+    if (cb.checked) {
+      geojsonLayer.addTo(map);
     }
+    else {
+      map.removeLayer(geojsonLayer);
+    }
+  }
 
 
-    var loadMap = function() {
+  var loadMap = function() {
 
-      // console.log("Loading map...");
+    // console.log("Loading map...");
 
-      map = L.map('map').setView([-0.1832911226129649, -78.48079204559326], 15);
+    map = L.map('map').setView([-0.1832911226129649, -78.48079204559326], 15);
 
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamExOTc5IiwiYSI6ImNpazcyZHFtcjAxOGJ2ZGt0NGNhamQ1cXQifQ.Kkz4bJY_fOE6PM9YaWzJIg', {
-        attribution: 'Map data &copy; <a href="http://openo streetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets'
-      }).addTo(map);
-
-
-
-      // console.log(map);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamExOTc5IiwiYSI6ImNpazcyZHFtcjAxOGJ2ZGt0NGNhamQ1cXQifQ.Kkz4bJY_fOE6PM9YaWzJIg', {
+      attribution: 'Map data &copy; <a href="http://openo streetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets'
+    }).addTo(map);
 
 
-      var routes;
 
-      //$.getJSON("http://192.168.43.240:5000/map/routes.json", function(data) {
-      // $.getJSON("http://localhost:5000/map/routes.json", function(data) {
-      //$.getJSON("http://192.168.0.107:5000/map/routes.json", function(data) {
-      $.getJSON("http://api-reciveci.rhcloud.com/map/routes.json", function(routes) {
-        // routes = data ;
-        //console.log(routes);
+    // console.log(map);
 
-        var routesData = JSON.stringify(routes);
-        localStorage.setItem('routesData', routesData);
 
-        // console.log(routesData);
+    var routes;
 
+    //$.getJSON("http://192.168.43.240:5000/map/routes.json", function(data) {
+    // $.getJSON("http://localhost:5000/map/routes.json", function(data) {
+    //$.getJSON("http://192.168.0.107:5000/map/routes.json", function(data) {
+    $.getJSON("http://api-reciveci.rhcloud.com/map/routes.json", function(routes) {
+      // routes = data ;
+      //console.log(routes);
+
+      var routesData = JSON.stringify(routes);
+      localStorage.setItem('routesData', routesData);
+
+      // console.log(routesData);
+
+      geojsonLayer = L.geoJson(routes, {
+        style: getStyle,
+        onEachFeature: onEachFeature
+      });
+
+
+      geojsonLayer.addTo(map);
+      $("#layerWastePickers").prop('checked',true);
+
+
+      // Welcome popup
+      var mapAlertExecuted = localStorage.getItem('routeOpened');
+      if (mapAlertExecuted == null) {
+        localStorage.setItem('routeOpened', 'Y');
+        popup = L.popup()
+                    .setLatLng([-0.18292634404976632, -78.47974061965942])
+                    .setContent("Haz un tab sobre cada línea para</br>conocer a <strong>l@s reciclador@s<strong>")
+                    .openOn(map);
+      }
+
+
+
+
+    }).fail(function() {
+
+      // Retrieve data from cache
+      var routes = JSON.parse(localStorage.getItem('routesData'));
+
+      if (routes != null) {
         geojsonLayer = L.geoJson(routes, {
           style: getStyle,
           onEachFeature: onEachFeature
         });
-        //geojsonLayer.addTo(map);
+
+        geojsonLayer.addTo(map);
+        $("#layerWastePickers").prop('checked',true);
+        
+      } else {
+        console.log(error);
+      }
+
+    });
+
+
+  }
 
 
 
 
-      }).fail(function() {
 
-        // Retrieve data from cache
-        var routes = JSON.parse(localStorage.getItem('routesData'));
+  function getStyle(feature) {
+    return {color: feature.properties.color,
+            weight: feature.properties.weight,
+            opacity: feature.properties.opacity};
+  }
 
-        if (routes != null) {
-          geojsonLayer = L.geoJson(routes, {
-            style: getStyle,
-            onEachFeature: onEachFeature
-          });
-        } else {
-          console.log(error);
-        }
 
+
+
+  function onEachFeature(feature, layer) {
+
+    //console.log(layer);
+
+
+    if (feature.properties && feature.properties.schedule) {
+
+      var container = $('<div />');
+
+      // Delegate all event handling for the container itself and its contents to the container
+      container.on('click', '.profile-link', function() {
+        $scope.openProfile(feature);
       });
 
+      // Insert whatever you want into the container, using whichever approach you prefer
+      container.html("<div class='map-popup'>" +
+                  "<a class='profile-link' href='#'>" +
+                  // "<div class='centered profile-icon' style='color: " + feature.properties["color"] + "; opacity: 0.4;'><i class='icon ion-android-walk'></i></div>" +
+                  "<div class='centered'><img class='wastepickerpic' src='" + feature.properties["waste_picker-image_url"] + "'/></div>" +
+                  "<div class='centered map-profile-name' style='color: " + feature.properties["color"] + "; opacity: 0.5;'>@" + feature.properties["waste_picker-name"] + "</div>" +
+                  "</a>" +
+                  "</div>");
+      //container.append(feature.properties["schedule"]);
+
+      // Insert the container into the popup
+      layer.bindPopup(container[0]);
+      // layer.bindPopup("Hello!").openPopup();
+      //layer.openPopup();
+
 
     }
+  }
 
 
 
 
-
-    function getStyle(feature) {
-      return {color: feature.properties.color,
-              weight: feature.properties.weight,
-              opacity: feature.properties.opacity};
-    }
-
-
-
-
-    function onEachFeature(feature, layer) {
-
-      //console.log(layer);
-
-
-      if (feature.properties && feature.properties.schedule) {
-
-        var container = $('<div />');
-
-        // Delegate all event handling for the container itself and its contents to the container
-        container.on('click', '.profile-link', function() {
-          $scope.openProfile(feature);
-        });
-
-        // Insert whatever you want into the container, using whichever approach you prefer
-        container.html("<div class='map-popup'>" +
-                    "<a class='profile-link' href='#'>" +
-                    // "<div class='centered profile-icon' style='color: " + feature.properties["color"] + "; opacity: 0.4;'><i class='icon ion-android-walk'></i></div>" +
-                    "<div class='centered'><img class='wastepickerpic' src='" + feature.properties["waste_picker-image_url"] + "'/></div>" +
-                    "<div class='centered map-profile-name' style='color: " + feature.properties["color"] + "; opacity: 0.4;'>@" + feature.properties["waste_picker-name"] + "</div>" +
-                    "</a>" +
-                    "</div>");
-        //container.append(feature.properties["schedule"]);
-
-        // Insert the container into the popup
-        layer.bindPopup(container[0]);
-        // layer.bindPopup("Hello!").openPopup();
-        //layer.openPopup();
-
-
-      }
-    }
-
-
-
-
-    $timeout(function(){
-      // console.log("Cargando...");
-      loadMap();
-    },100);
+  $timeout(function(){
+    // console.log("Cargando...");
+    loadMap();
+  },100);
 
 
 
--- www/scripts/recycling-controller.js
+++ www/scripts/recycling-controller.js
@@ -4,11 +4,33 @@ myApp.controller('RecyclingController', function($scope, $recycling, $timeout) {
     $scope.categories_2 = [];
     $scope.success = true;
 
+    var getCurrentDate = function() {
+      var dt = new Date();
+      return dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate();
+    }
 
 
-    var loadData = function() {
-      $scope.success = true;
-      modal.show();
+    var loadFromCache = function() {
+
+      console.log("Loading from cache...");
+
+      var separate = JSON.parse(localStorage.getItem('separateData'));
+
+      if (separate != null) {
+        $scope.separate = separate;
+      } else {
+        $scope.success = false;
+      }
+      modal.hide();
+
+    }
+
+
+    var refreshData = function(currentDate) {
+
+      console.log('Refreshing data...');
+
+      localStorage.setItem('lastRecyclingRefreshDate', currentDate);
 
       $recycling.separate(function(separate) {
 
@@ -23,21 +45,43 @@ myApp.controller('RecyclingController', function($scope, $recycling, $timeout) {
 
       },
       function(error) {
+          console.log('Error:');
           console.log(error);
           // alert('Error: ' + error);
 
-          // Retrieve data from cache
-          var separate = JSON.parse(localStorage.getItem('separateData'));
-
-
-          if (separate != null) {
-            $scope.separate = separate;
-          } else {
-            $scope.success = false;
-          }
-          modal.hide();
+          loadFromCache();
 
       });
+    }
+
+
+
+    $scope.loadDataPull = function($done) {
+        // loadData($done);
+        var currentDate = getCurrentDate();
+        refreshData(currentDate);
+        $done();
+    };
+
+
+    var loadData = function() {
+      $scope.success = true;
+      modal.show();
+
+
+      var currentDate = getCurrentDate();
+      // console.log('currentDate:' + currentDate);
+
+      var lastRecyclingRefreshDate = localStorage.getItem('lastRecyclingRefreshDate');
+      // console.log('lastRecyclingRefreshDate:' + lastRecyclingRefreshDate);
+      if (lastRecyclingRefreshDate == null || lastRecyclingRefreshDate != currentDate) {
+        // console.log("Must refresh");
+        refreshData(currentDate);
+
+      } else {
+        loadFromCache();
+      }
+
 
     }
 
@@ -46,7 +90,6 @@ myApp.controller('RecyclingController', function($scope, $recycling, $timeout) {
     $timeout(function(){
 
       loadData();
-
 
     },100);
 
