24c6ffd6a6a8a6bd4d17a9800b5a71a073453250
Jefferson Morocho <morochoji5@hotmail.com>
2016-07-11 08:30:04 -0500

    solucion al error --invalid key at column 2 --error en el news.html

--- www/scripts/affiliations-service.js
+++ www/scripts/affiliations-service.js
@@ -3,8 +3,8 @@ myApp.factory('$affiliations', [ '$resource', function($resource) {
   // Use this line for Production env
   // var API_ROOT = 'http://api-reciveci.rhcloud.com/';
   // Use this line for Development env
-  // var API_ROOT = 'http://localhost:5000/';
-  var API_ROOT = 'http://192.168.0.112:5000/';
+  var API_ROOT = 'http://localhost:5000/';
+  //var API_ROOT = 'http://192.168.0.112:5000/';
 
   return $resource(API_ROOT, {}, {
       last : {
